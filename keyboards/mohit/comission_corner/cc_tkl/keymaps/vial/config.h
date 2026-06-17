5c9eac11f86585dd70814c0d349cf9b40c2ba903
unimplanted <y4man99@gmail.com>
2026-06-17 19:13:04 +0900

    comission_corner

---
+++ keyboards/mohit/comission_corner/cc_tkl/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+ /* SPDX-License-Identifier: GPL-2.0-or-later */
+ 
+ #pragma once
+ 
+ #define VIAL_KEYBOARD_UID {0x71, 0xF4, 0xB6, 0x6B, 0xC4, 0x6E, 0x65, 0x61}
+
+ #define DYNAMIC_KEYMAP_LAYER_COUNT 8
+ 
+ #ifndef RGBLIGHT_ENABLE
+    #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+    #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+ #endif
---
+++ keyboards/mohit/comission_corner/cc_tkl/keymaps/vial/rules.mk
@@ -0,0 +1,21 @@
+# VIA/Vial 활성화
+  VIA_ENABLE = yes
+  VIAL_ENABLE = yes
+  DYNAMIC_KEYMAP_ENABLE = yes
+  VIAL_INSECURE = yes
+
+ # 링크 타임 최적화 (펌웨어 크기 줄이기)
+ LTO_ENABLE = yes
+
+ # 기본 기능들
+ MOUSEKEY_ENABLE = no         # 마우스 키 필요시 yes
+ EXTRAKEY_ENABLE = yes        # 미디어 키 필요시 yes
+
+ # 고급 기능 (필요시 활성화)
+ TAP_DANCE_ENABLE = yes       # Tap Dance
+ COMBO_ENABLE = yes           # Combo
+ KEY_OVERRIDE_ENABLE = yes    # Key Override
+ LEADER_ENABLE = no           # Leader Key
+ NO_MK_MK = no                # Macro
+
+ RGBLIGHT_ENABLE = yes
